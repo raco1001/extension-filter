f343b3d58d2f4502a4c748ba451c0b081e86ff88
김종현 <jhk9027@gmail.com>
2025-07-02 21:49:40 +0900

    Fix: init - Github 대소문자 오류

--- lib/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-
-if (!process.env.DIRECT_URL) {
-  throw new Error('DATABASE_URL is not set')
-}
-
-const client = postgres(process.env.DIRECT_URL, { max: 1 })
-export const db = drizzle(client)
