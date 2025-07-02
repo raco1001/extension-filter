94a1dc740226746b764a970e3ab51f2929baaec9
김종현 <jhk9027@gmail.com>
2025-07-02 13:39:21 +0900

    fix: delete pgbouncer

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,9 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
-if (!process.env.DATABASE_URL) {
+if (!process.env.DIRECT_URL) {
   throw new Error('DATABASE_URL is not set')
 }
 
-const client = postgres(process.env.DATABASE_URL, { max: 1 })
+const client = postgres(process.env.DIRECT_URL, { max: 1 })
 export const db = drizzle(client)
